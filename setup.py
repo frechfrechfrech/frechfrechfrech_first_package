831dd9da263503c84028c223fd0d3c431cbc59f6
Alex Frech <frechfrech2@gmail.com>
2019-03-28 10:49:26 -0600

    set up __init__.py and setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='frechfrechfrech_first_package',
+      version='0.1',
+      description='My first package attempt',
+      url='https://github.com/frechfrechfrech/frechfrechfrech_first_package',
+      author='Alex Frech',
+      author_email='frechfrechfrech@gmail.com',
+      license='MIT',
+      packages=['frechfrechfrech_first_package'],
+      zip_safe=False)
